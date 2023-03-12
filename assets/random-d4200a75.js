0f7b080de84a78687c27676ab55689f58716082c
XiaoQiGe7 <68734157+XiaoQiGe7@users.noreply.github.com>
2023-03-12 16:45:52 +0800

    Add files via upload

---
+++ assets/random-d4200a75.js
@@ -0,0 +1,1 @@
+function u(o=32){const t="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";let r="";for(let n=0;n<o;n++)r+=t.charAt(Math.floor(Math.random()*t.length));return r}export{u};
